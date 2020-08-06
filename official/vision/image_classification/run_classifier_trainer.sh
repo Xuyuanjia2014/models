bc1e85c7a10952f8934cb66b425d9605dc610b3c
Xuyuanjia2014 <794774196@qq.com>
2020-08-06 17:07:39 +0800

    modified imagenet 2012 datasets preparation

--- official/vision/image_classification/run_classifier_trainer.sh
+++ official/vision/image_classification/run_classifier_trainer.sh
@@ -1,5 +1,9 @@
 # 要先切换到py36环境： python conda activate py36
+# 准备数据放入：/root/tensorflow_datasets/downloads/manual
 export PYTHONPATH=$PYTHONPATH:/root/models
 export MODEL_DIR=/root/disk/tensorflow-model
 export DATA_DIR=/root/disk/tensorflow-datasource
+
+# 需要执行数据预处理
+python -m tensorflow_datasets.scripts.download_and_prepare --datasets=imagenet2012
 python3 classifier_trainer.py --mode=train_and_eval --model_type=resnet --dataset=imagenet --model_dir=$MODEL_DIR --data_dir=$DATA_DIR --config_file=configs/examples/resnet/imagenet/gpu.yaml --params_override='runtime.num_gpus=0'
--- official/vision/image_classification/run_mnist_main.sh
+++ official/vision/image_classification/run_mnist_main.sh
@@ -1,10 +1,4 @@
 # 要先切换到py36环境： python conda activate py36
 export MODEL_DIR=/root/disk/tensorflow-model
 export DATA_DIR=/root/disk/tensorflow-datasource
-python3 mnist_main.py \
-  --model_dir=$MODEL_DIR \
-  --data_dir=$DATA_DIR \
-  --train_epochs=10 \
-  --distribution_strategy=one_device \
-  --num_gpus=1 \
-  --download
+python3 mnist_main.py --model_dir=$MODEL_DIR  --data_dir=$DATA_DIR --train_epochs=10 --distribution_strategy=one_device --num_gpus=1 --download
